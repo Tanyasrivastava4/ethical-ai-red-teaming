528120725be3c8df22155c588db4bd460a87d83b
Tanya srivastava <130232646+Tanyasrivastava4@users.noreply.github.com>
2025-11-05 11:47:36 +0530

    constant.py

--- src/constant.py
+++ src/constant.py
@@ -1,8 +1,8 @@
 # src/constants.py
 import yaml, os
-cfg_path = "configs/config.yml"
+cfg_path = "configs/config.yaml"
 if not os.path.exists(cfg_path):
-    raise FileNotFoundError("Please create configs/config.yml")
+    raise FileNotFoundError("Please create configs/config.yaml")
 
 with open(cfg_path, "r") as f:
-    CFG = yml.safe_load(f)
+    CFG = yaml.safe_load(f)
